f91b160e9d820ca42a351a9065c62fa43569ac84
BIKTOPBB <BIKTOPBB@gmail.com>
2018-11-13 23:00:07 +0200

    2

--- app.jsx
+++ app.jsx
@@ -7,14 +7,45 @@
 
 
 // это функциональный стиль создания компонента в React
-function Hero(props) {
-    return (
-        <div className='container'>
-            <img width="200" src={props.srcUrl}></img>
-            <h1>{props.title}</h1>
-            <p>{props.subtitle}</p>
-        </div>);
-}
+// function Hero(props) {
+//     let count = 0;
+//     function handleClick() {
+//         // console.log('Click');
+//         count++;
+//         console.log(count);
+//     }
+//     return (
+//         <div className='container'>
+//             <div>{count}</div>
+//             <img width="200" src={props.srcUrl} onClick={handleClick}></img>
+//             <h1>{props.title}</h1>
+//             <p>{props.subtitle}</p>
+//         </div>);
+// }
+
+//это создание обычного компонента (есть возможность создавать состояния)
+// var Hero = React.createClass({
+class Hero extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { count: 0 }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.setState({ count: ++this.state.count });
+        console.log(this.state.count);
+    }
+    render() {
+        return (
+            <div className='container'>
+                <div>{this.state.count}</div>
+                <img width="200" src={this.props.srcUrl} onClick={this.handleClick}></img>
+                <h1>{this.props.title}</h1>
+                <p>{this.props.subtitle}</p>
+            </div>
+        );
+    }
+};
 
 // ReactDOM.render(React.createElement(Hero), document.getElementById('root')); // или
 ReactDOM.render(
